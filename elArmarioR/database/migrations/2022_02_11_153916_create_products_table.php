9d666f0982baf6ec8f860000da4a3cacce88984d
[rugren] <[rubgarciaruiz@gmail.com]>
2024-01-27 14:23:35 +0100

    Sustituidos los $table->integer por $table->double (para que sean como un float, que se pueda poner 00,00 € a los precios y al balance nuestro) SOLO PUESTO A LO QUE LLEVE PRECIO, al 'price', no a la cantidad 'quantity'.

--- elArmarioR/database/migrations/2022_02_11_153916_create_products_table.php
+++ elArmarioR/database/migrations/2022_02_11_153916_create_products_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description');
             $table->string('image');
-            $table->integer('price');
+            $table->double('price');
             $table->timestamps();
         });
     }
